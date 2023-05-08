37ed8f46a84441aaf9fb795742c958dedc393561
GuysRGithub <guysdeveloperrobot@gmail.com>
2023-05-08 15:33:41 +0700

    Add welcome page

---
+++ lib/pages/frame/welcome/view.dart
@@ -0,0 +1,33 @@
+import 'package:chatty/common/values/colors.dart';
+import 'package:chatty/pages/frame/welcome/controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+class WelcomePage extends GetView<WelcomeController> {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryElement,
+      body: Center(
+        child: Container(
+          child: _buildHeadTitle(controller.title),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeadTitle(String title) {
+    return Text(
+      title,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+          color: AppColors.primaryElementText,
+          fontWeight: FontWeight.bold,
+          fontSize: 45.0.sp,
+          fontFamily: "Montserrat"),
+    );
+  }
+}
