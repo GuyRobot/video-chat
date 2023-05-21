a7e7ed84f4425a85311a847f52930318e303791c
GuysRGithub <guysdeveloperrobot@gmail.com>
2023-05-21 11:14:20 +0700

    Add message controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:chatty/common/routes/names.dart';
 import 'package:chatty/common/routes/pages.dart';
 import 'package:chatty/common/style/theme.dart';
+import 'package:chatty/common/utils/FirebaseMassagingHandler.dart';
 import 'package:chatty/global.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
@@ -11,7 +14,23 @@ void main() async {
   await Global.init();
   SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  firebaseChatInit().whenComplete(() => FirebaseMessagingHandler.config());
   runApp(const MyApp());
+}
+
+Future firebaseChatInit() async {
+  FirebaseMessaging.onBackgroundMessage(
+      FirebaseMessagingHandler.firebaseMessagingBackground);
+  if (GetPlatform.isAndroid) {
+    FirebaseMessagingHandler.flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()!
+        .createNotificationChannel(FirebaseMessagingHandler.channel_call);
+    FirebaseMessagingHandler.flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()!
+        .createNotificationChannel(FirebaseMessagingHandler.channel_message);
+  }
 }
 
 class MyApp extends StatelessWidget {
--- lib/pages/frame/message/controller.dart
+++ lib/pages/frame/message/controller.dart
@@ -1,4 +1,7 @@
+import 'package:chatty/common/apis/apis.dart';
+import 'package:chatty/common/entities/base.dart';
 import 'package:chatty/pages/frame/message/state.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:get/get.dart';
 
 class MessageController extends GetxController {
@@ -8,6 +11,15 @@ class MessageController extends GetxController {
   @override
   void onReady() {
     super.onReady();
+    firebaseMessageSetup();
+  }
 
+  firebaseMessageSetup() async {
+    String? fcmToken = await FirebaseMessaging.instance.getToken();
+    if (fcmToken != null) {
+      BindFcmTokenRequestEntity requestEntity = BindFcmTokenRequestEntity(fcmtoken: fcmToken);
+
+      await ChatAPI.bind_fcmtoken(params: requestEntity);
+    }
   }
 }
